4a65cc116e3b5e93fc4c0dd8f982efea8c8604c1
Jose Oliveira <108938041+bremen-jose-oliveira@users.noreply.github.com>
2025-02-03 13:41:20 +0100

    add ned Identity

---
+++ src/main/java/com/Bibliotek/Personal/entity/FriendshipStatus.java
@@ -0,0 +1,7 @@
+package com.Bibliotek.Personal.entity;
+
+public enum FriendshipStatus {
+    PENDING,
+    ACCEPTED,
+    DECLINED
+}
---
+++ src/main/java/com/Bibliotek/Personal/entity/UserBook.java
@@ -0,0 +1,4 @@
+package com.Bibliotek.Personal.entity;
+
+public class UserBook {
+}
